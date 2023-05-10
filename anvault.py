d7893dd22e3ef710f7e06b370004552e2547fadd
Rajesh Kumar Kondapalli <6193256+rajeshkumarkondapalli@users.noreply.github.com>
2023-05-10 06:15:14 -0400

    Update anvault.py

--- anvault.py
+++ anvault.py
@@ -1,4 +1,6 @@
 from ansible.parsing.vault import VaultLib
+# import subprocess
+# import os
 
 password = "my_password"
 vault = VaultLib([(None, password)])
@@ -11,3 +13,12 @@ encrypted_data = vault.encrypt(plaintext)
 decrypted_data = vault.decrypt(encrypted_data)
 
 print(decrypted_data)
+
+
+
+# # subprocess.run(["powershell", "pwd"], shell=True)
+# # os.system("pwd")
+
+# # os.system('ansible-vault encrypt ' + 'config.yaml')
+# ansible-vault encrypt vault.yml --vault-password-file vault_pass.txt
+# ansible-vault decrypt vault.yml --vault-password-file vault_pass.txt
